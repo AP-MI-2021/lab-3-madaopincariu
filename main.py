fe4f84e56f5d0935e55bde62ec10df897eec4030
Mădălina Opincariu <91501449+madaopincariu@users.noreply.github.com>
2021-10-13 19:09:12 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -92,6 +92,43 @@ def get_longest_concat_is_prime(l):
 def test_get_longest_concat_is_prime():
     assert get_longest_concat_is_prime([2, 1, 4, 6]) ==[2]
 
+def is_palindrome(n):
+    '''
+    Determină dacă un număr dat este palindrom.
+    :param n: un numar
+    :return: True daca este palindrom sau False in caz contrar
+    '''
+    x=n
+    inv=0
+    while x!=0:
+        c=x%10
+        inv=inv*10+c
+        x=x//10
+    if inv==n:
+        return True
+    return False
+
+def test_is_palindrome():
+    assert is_palindrome(121) is True
+    assert is_palindrome(97) is False
+
+def get_longest_all_palindromes(l):
+    '''
+    Determina ce mai lunga subsecventa a listei care contine doar numere de tip palindrom
+    :param l: lista de numere
+    :return: lista de numere
+    '''
+    rezultat = []
+    for i in range(len(l)):
+        for j in range(i, len(l)):
+            if is_palindrome(l[i:j + 1]) and len(l[i:j + 1]) > len(rezultat):
+                rezultat = l[i:j + 1]
+    return rezultat
+
+def test_get_longest_all_palindromes():
+    assert get_longest_all_palindromes([121, 11, 343, 12]) == [121, 11, 343]
+    assert get_longest_all_palindromes([45, 56, 3875, 1221])== [1221]
+
 def main():
     l=[]
     test_nr_prim()
@@ -99,20 +136,26 @@ def main():
     test_nr_pare()
     test_get_longest_concat_is_prime()
     test_concat_num_din_lista()
+    test_is_palindrome()
+    test_get_longest_all_palindromes()
     while True:
         print('1.Citire lista')
         print('2.Afiseaza cea mai lunga secventa de numere pare')
         print('3.Afiseaza cea mai lunga secventa care prin concatenare reprezinta un numar prim ')
+        print('4.Afseaza cea mai lunga secventa in care toate numerele sunt palindroame')
         print('x.Iesire din program')
         opt = input('Alege optiunea: ')
         if opt=='1':
             l=citire_lista()
         elif opt == '2':
-            l = int(input('Dati lista de numere:'))
+            l = input('Dati lista de numere:')
             print(get_longest_all_even(l))
-        elif opt == '2':
-            l = int(input('Dati lista de numere:'))
+        elif opt == '3':
+            l =input('Dati lista de numere:')
             print(get_longest_concat_is_prime(l))
+        elif opt=='4':
+            l = input('Dati lista de numere:')
+            print(get_longest_all_palindromes(l))
         elif opt == 'x':
             break
         else:
